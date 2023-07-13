8ac4b4070c14bfc95f6628c838debde1f3a397b4
leosun <leosunsl@outlook.com>
2023-07-14 06:44:56 +0800

    equip with ceedling test framework

---
+++ test/test_SoftTimer.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "SoftTimer.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_SoftTimer_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement SoftTimer");
+}
+
+#endif // TEST
