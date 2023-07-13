0b5851aa06324bee16355208a10f6a6b638e0d2c
leosun <leosunsl@outlook.com>
2023-07-14 07:07:15 +0800

    plan some tests

--- test/test_SoftTimer.c
+++ test/test_SoftTimer.c
@@ -12,9 +12,44 @@ void tearDown(void)
 {
 }
 
-void test_SoftTimer_NeedToImplement(void)
+void test_SoftTimer_TimerListShouldOnlyHaveHeadAfterInit(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement SoftTimer");
+    TEST_IGNORE_MESSAGE("test_SoftTimer_TimerListShouldOnlyHaveHeadAfterInit");
+}
+
+void test_SoftTimer_TimerHeadShouldHaveNoDataAfterInit(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_TimerHeadShouldHaveNoDataAfterInit");
+}
+
+void test_SoftTimer_TimerHeadShouldBeNullIfNoTimerSource(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_TimerHeadShouldBeNullIfNoTimerSource");
+}
+
+void test_SoftTimer_CreateFailIfNotInit(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_CreateFailIfNotInit");
+}
+
+void test_SoftTimer_NewCreateStayInListTail(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_NewCreateStayInListTail");
+}
+
+void test_SoftTimer_KillFailIfNotInit(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_KillFailIfNotInit");
+}
+
+void test_SoftTimer_KillFailIfDeleteInvalidTimer(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_KillFailIfDeleteInvalidTimer");
+}
+
+void test_SoftTimer_TimerListRemainIntegratedAfterKill(void)
+{
+    TEST_IGNORE_MESSAGE("test_SoftTimer_TimerListRemainIntegratedAfterKill");
 }
 
 #endif // TEST
